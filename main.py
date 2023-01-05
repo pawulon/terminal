031a91061132b4ffc9540fdbda9930a6bf2ddcfb
pawulon <pawelryndak@gmail.com>
2023-01-05 15:29:59 +0100

    Add clock

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ pygame.init()
 
 pygame.display.set_caption('Terminal')
 screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
 
 
-def handle_quit_event(event):
+def handle_events() -> None:
+    for event in pygame.event.get():
+        handle_quit_event(event)
+
+
+def handle_quit_event(event: pygame.event.Event) -> None:
     if event.type == pygame.QUIT:
         handle_quit()
 
 
-def handle_quit():
+def handle_quit() -> None:
     pygame.quit()
     sys.exit()
 
 
 def main():
     while True:
-        for event in pygame.event.get():
-            handle_quit_event(event)
+        handle_events()
         pygame.display.update()
+        clock.tick(60)
 
 
 if __name__ == '__main__':
