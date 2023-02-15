8ea785b947ecd84f5ff3077afa957a99d960e861
pawulon <pawelryndak@gmail.com>
2023-02-15 09:08:40 +0100

    Add background and try out things

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import asyncio
 import pygame
 import sys
 
 pygame.init()
 
 pygame.display.set_caption('Terminal')
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1222, 653))
 clock = pygame.time.Clock()
+background = pygame.image.load('assets/background.jpg')
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+terminal_font = pygame.font.SysFont('Consolas', 50)
+text_surface = terminal_font.render('Hello, hi! I am that guy.', False, 'Gray')
 
 
 def handle_events() -> None:
@@ -23,12 +29,20 @@ def handle_quit() -> None:
     sys.exit()
 
 
-def main():
+def draw_surface():
+    screen.blit(background, (0, 0))
+    screen.blit(test_surface, pygame.mouse.get_pos())
+    screen.blit(text_surface, (300, 500))
+
+
+async def main():
     while True:
         handle_events()
+        draw_surface()
         pygame.display.update()
         clock.tick(60)
+        await asyncio.sleep(0)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
